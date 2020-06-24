54d77346adab2ce1139638a7cb23118a77e96bd6
Janet <janetkarpenske@gmail.com>
2020-06-24 11:24:51 -0700

    Failed test to recieve correct array for second line

--- tests/haiku-checker.test.js
+++ tests/haiku-checker.test.js
@@ -6,6 +6,7 @@ describe('Haiku', () => {
   test('should correctly store arrays as variables', () => {
     const haiku = new Haiku(["Dogs are very cute"], ["They really like to get treats"], ["They are such good boys"]);
     expect (haiku.sentence1).toEqual(["Dogs are very cute"]);
+    expect (haiku.sentence2).toEqual(["They really like to get treats"]);
 
   });
 
