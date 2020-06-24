efe7b7e7ecd2bb654b1774259b97f8294a09fd85
Janet <janetkarpenske@gmail.com>
2020-06-24 11:27:48 -0700

    Passed test to recieve correct input from third line

--- src/haiku-checker.js
+++ src/haiku-checker.js
@@ -2,5 +2,5 @@ export class Haiku {
   constructor (sentence1, sentence2, sentence3) {
     this.sentence1 = sentence1;
     this.sentence2 = sentence2;
-    this.sentence3 = sentence1;
+    this.sentence3 = sentence3;
 }}
